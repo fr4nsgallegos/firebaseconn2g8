458686bfb57e89dbd17a49bb1dd85ba63f975ebb
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-07-13 12:09:58 -0500

    stream page init

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,108 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebaseconn2g8/pages/stream_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  CollectionReference usersReference =
+      FirebaseFirestore.instance.collection("users");
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Home"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  usersReference.get().then((value) {
+                    value.docs.forEach((e) {
+                      print(e.data());
+                    });
+                  });
+                },
+                child: Text("Get data"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  usersReference
+                      .where("nombre", isEqualTo: "Pedro")
+                      .where("estatura", isGreaterThan: 175)
+                      .get()
+                      .then(
+                    (value) {
+                      QuerySnapshot userCollection = value;
+                      List<QueryDocumentSnapshot> docs = userCollection.docs;
+                      docs.forEach((e) {
+                        print(e.id);
+                        print(e.data());
+                      });
+                    },
+                  );
+                },
+                child: Text("Traer Data FILTRADA"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  usersReference.add({
+                    'nombre': "Pedro",
+                    "apellido": "Fernandez",
+                    "estatura": 178,
+                    "esPeruano": true,
+                  }).then((value) {
+                    print(value.id);
+                  });
+                },
+                child: Text("Agregar usuario"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  usersReference.doc("idNoExiste").set(
+                    {
+                      'nombre': "Javier",
+                      "apellido": "Mader",
+                      "estatura": 198,
+                      "esPeruano": false,
+                    },
+                  );
+                },
+                child: Text("Inserción 2"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  usersReference.doc("idNoExiste").update(
+                    {
+                      "nombre": "Anastacio",
+                    },
+                  );
+                },
+                child: Text("Actualización"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  usersReference.doc("idNoExiste").delete();
+                },
+                child: Text("Eliminar"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => StreamPage(),
+                    ),
+                  );
+                },
+                child: Text("Stream page"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/stream_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StreamPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stream page"),
+      ),
+    );
+  }
+}
