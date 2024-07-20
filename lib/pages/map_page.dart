d39e5c85bc7dd1bd1694fd8bdf23b0355fbd8a69
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-07-20 12:28:39 -0500

    set markers

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:logger/logger.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  Logger _logger = Logger();
+
+  Set<Marker> _markers = {};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
           target: LatLng(-12.051106645118788, -76.9591172953207),
-          zoom: 5,
+          zoom: 15,
         ),
+        onTap: (LatLng position) {
+          // print(position);
+          _logger.d(position);
+          _markers.add(
+            Marker(
+              markerId: MarkerId(_markers.length.toString()),
+              position: position,
+            ),
+          );
+          setState(() {});
+        },
+        markers: _markers,
+        // markers: {
+        //   Marker(
+        //     markerId: MarkerId("firstMarker"),
+        //     position: LatLng(-12.051106645118788, -76.9591172953207),
+        //   ),
+        // },
       ),
     );
   }
