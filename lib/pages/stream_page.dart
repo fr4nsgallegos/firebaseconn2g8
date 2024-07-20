a3fa8d8eb0b1012ff58e416266ae209621cb9a44
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-07-19 19:43:34 -0500

    stream normal

--- lib/pages/stream_page.dart
+++ lib/pages/stream_page.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 
-class StreamPage extends StatelessWidget {
+class StreamPage extends StatefulWidget {
   const StreamPage({super.key});
+
+  @override
+  State<StreamPage> createState() => _StreamPageState();
+}
+
+class _StreamPageState extends State<StreamPage> {
+  Future<String> getData() async {
+    await Future.delayed(Duration(seconds: 5));
+    return "Esta es la data";
+  }
+
+  @override
+  void initState() {
+    print("inicio de stream");
+    Stream<String> myStream = Stream.fromFuture(getData());
+    print("Stream creado");
+    myStream.listen((data) {
+      print("la data es: $data");
+    }, onDone: () {
+      print("Task Done");
+    });
+    print("FINISH");
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
